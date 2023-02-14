54221b786f14780d3b988e76d92b0a1388642182
crashtellini <107076865+crashtellini@users.noreply.github.com>
2023-02-13 17:02:05 -0800

    Made the url constant, testing Ercot response, making sure csv file is being created

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,35 @@ use csv::Writer;
 use scraper::{Html, Selector};
 use reqwest::blocking::Client;
 
+const URL: &str = "https://www.ercot.com/content/cdr/html/20230213_dam_spp.html";
+
+
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Start");
 
     // Get Response from ERCOT
-    
     let client = Client::new();
-    let response = client.get("https://www.ercot.com/content/cdr/html/20230213_dam_spp.html")
+    let response = client.get(URL)
         .send()?
         .text()?;
-    
+
       //Parse the data
     let html = Html::parse_document(&response);
+    
+
+    println!("HTML:\n{}", response);
 
     //Prepare the output
 
     //Write the data to csv file
     let file = File::create("output.csv")?;
     let mut writer = Writer::from_writer(file);
-   
-    writer.write_record(&["Column 1", "Column 2", "Value 1", "Value 2"])?;
+    
+    
+    writer.write_record(&["Column 1", "Column 2", "Value 1", "Value 2"])?; //test 
 
-   
     writer.flush()?;
-   
-   
+    
     println!("End");
     Ok(())
 }
